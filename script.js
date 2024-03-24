d950452a4514d01dac7572cf23933692c32bc690
anirbansaha2002 <98881271+anirbansaha2002@users.noreply.github.com>
2024-03-24 12:41:34 +0530

    Add files via upload
    Updated script.js and index.html accordingly

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 let date = new Date();
-let hours = date.getHours();
-let mins = date.getMinutes();
-let secs = date.getSeconds();
-
 let today = date.getDate();
 let month = date.getMonth();
 let year = date.getFullYear();
@@ -13,32 +9,13 @@ document.getElementsByClassName("today")[0].innerHTML = today;
 document.getElementsByClassName("month")[0].innerHTML = months[month];
 document.getElementsByClassName("year")[0].innerHTML = year;
 
-document.getElementsByClassName("hours")[0].innerHTML = updateNumber(hours);
-document.getElementsByClassName("mins")[0].innerHTML = updateNumber(mins);
-document.getElementsByClassName("secs")[0].innerHTML = updateNumber(secs);
+setInterval(myFunction ,100);
 
-intervalID = setInterval(updateTime ,1000);
-
-function updateNumber(number){
-    if(number >= 0 && number<10){
-        number = "0"+ number;
-    }
-    return number;
-}
-
-function updateTime(){
-    secs += 1;
-    if(secs > 59){
-        mins += 1;
-        secs = 0;
-    }
-    if(mins > 59){
-        hours += 1;
-        mins = 0;
-    }
-    hours = hours % 24;
-    document.getElementsByClassName("hours")[0].innerHTML = updateNumber(hours);
-    document.getElementsByClassName("mins")[0].innerHTML = updateNumber(mins);
-    document.getElementsByClassName("secs")[0].innerHTML = updateNumber(secs);
-}
+function myFunction() {
+    let time = new Date();
+    document.getElementsByClassName("time")[0].innerHTML=
+    time.getHours() + " : " +
+    time.getMinutes() + " : " +
+    time.getSeconds();
+  }
 
